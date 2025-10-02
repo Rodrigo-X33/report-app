d6684603de8a3bb956b821eca23ac6a7c906bf95
Rodrigo-X33 <rodrigo.chaconx33@gmail.com>
2025-10-02 10:00:51 -0600

    fix: supabase database connection

--- src/main/java/com/mycompany/db/Database.java
+++ src/main/java/com/mycompany/db/Database.java
@@ -11,9 +11,8 @@ public class Database {
 
     protected Connection conexion;
     private final String JDBC_DRIVER = "org.postgresql.Driver";
-    private final String DB_URL = "jdbc:postgresql://lvldoxjkilisktjsycrs.supabase.co:5432/ilib";
-
-    private final String USER = "postgres";
+    private final String DB_URL = "jdbc:postgresql://aws-1-us-east-2.pooler.supabase.com:6543/postgres";
+    private final String USER = "postgres.lvldoxjkilisktjsycrs";
     private final String PASS = "VZRX^uX*T@7x";
 
     public void Conectar() throws ClassNotFoundException {
